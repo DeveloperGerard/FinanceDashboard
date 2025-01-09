919642ae65b4314332222e711159693600f68547
= <=>
2025-01-09 12:33:39 -0300

    create account table

---
+++ app/models/account.py
@@ -0,0 +1,9 @@
+from app import db 
+
+class Account(db.model):
+    __tablename__ = "accounts"
+    id            = db.Column(db.Integer,primary_key=True)
+    account_name  = db.Column(db.String(50),nullable=False)
+    type          = db.Column(db.String(50),nullable=False)
+    user_id       = db.Column(db.Integer,db.ForeignKey("users.id",ondelete="CASCADE"))
+    user          = db.relationship("User",back_populates="accounts")
