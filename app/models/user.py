85e88c4c346bd6f5a22a3676a8cd57a4d4ce7a2c
= <=>
2025-01-09 11:31:54 -0300

    add balance column to user table

--- app/models/user.py
+++ app/models/user.py
@@ -3,11 +3,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin 
  
 class User(UserMixin, db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
-    username = db.Column(db.String(64), unique=True, nullable=False) 
-    email = db.Column(db.String(120), unique=True, nullable=False) 
-    password_hash = db.Column(db.String(128)) 
- 
+    __tablename__  = "users"
+    id             = db.Column(db.Integer, primary_key=True) 
+    username       = db.Column(db.String(64), unique=True, nullable=False) 
+    email          = db.Column(db.String(120), unique=True, nullable=False) 
+    password_hash  = db.Column(db.String(128)) 
+    balance        = db.Column(db.Integer)
     def set_password(self, password): 
         self.password_hash = generate_password_hash(password) 
  
