7e510bcc6578004e87d31ea9d99d8a06949c0690
= <=>
2025-01-13 15:50:45 -0300

    Add delete_user,update_user in user_controller.py

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,16 +1,27 @@
 from ..models.user import  User
 from app import db
 
-class ControladorUsuarios:
+class UserController:
     @staticmethod
-    def crear_usuario(nombre,correo,clave):
+    def create_user(username,email,password):
         usuario = User()
-        usuario.username = nombre
-        usuario.email = correo 
-        usuario.set_password(clave)
+        usuario.username = username
+        usuario.email = email
+        usuario.set_password(password)
         usuario.balance = 0
         db.session.add(usuario)
         db.session.commit()
         return usuario
+    
+    @staticmethod
+    def delete_user(usuario:object):
+        db.session.delete(usuario)
+        db.session.commit()
+        return usuario
+    
+    @staticmethod
+    def update_user(usuario:object):
+        db.session.add(usuario)
+        db.session.commit()
 
         
