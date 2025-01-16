08549e94f7ff6b65e5d38ffc1dcf27d2ab30014e
= <=>
2025-01-16 10:23:14 -0300

    fix confirm password not working

--- app/forms/form_user.py
+++ app/forms/form_user.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired, Email,Length,EqualTo
 class FormularioRegistro(FlaskForm):
     nombre               = StringField('Nombre',validators=[DataRequired(),Length(min=3,max=64)])
     email                = EmailField('Gmail',validators=[DataRequired(),Email()])
-    clave           = PasswordField('clave',validators=[DataRequired(),Length(min=8,max=128),EqualTo('confirmar_clave',message="Las claves deben ser iguales")])
-    confirmar_clave = PasswordField('Confirmar clave',validators=[DataRequired()])
+    clave                = PasswordField('clave',validators=[DataRequired(),Length(min=8,max=128),EqualTo('confirmar_clave',message="Las claves deben ser iguales")])
+    confirmar_clave      = PasswordField('Confirmar clave',validators=[DataRequired(),Length(min=8,max=128)])
     submit               = SubmitField('Registrarse')
 
 class FormularioInicio(FlaskForm):
     email      = EmailField('Gmail',validators=[DataRequired(),Email()])
-    clave = PasswordField('clave',validators=[DataRequired(),Length(min=8,max=128)])
+    clave      = PasswordField('clave',validators=[DataRequired(),Length(min=8,max=128)])
     submit     = SubmitField("Iniciar sesion")
