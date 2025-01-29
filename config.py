c38486dc83c22e5b35b82c242b82b57bd3271595
= <=>
2025-01-29 15:32:56 -0300

    Move settings from server.py to config.py

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config:
     SECRET_KEY = "adlasdlalsdlasdlel"
     SQLALCHEMY_DATABASE_URI ="mysql://root@localhost/finance"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT  = 465
+    MAIL_USERNAME = 'dashboardfinance1@gmail.com'
+    MAIL_PASSWORD = 'vtow xsqo qico odwj'
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
--- server.py
+++ server.py
@@ -11,12 +11,6 @@ app.register_blueprint(user)
 app.register_blueprint(user_functions)
 app.register_blueprint(public)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'dashboardfinance1@gmail.com'
-app.config['MAIL_PASSWORD'] = 'vtow xsqo qico odwj'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 if __name__=="__main__":
