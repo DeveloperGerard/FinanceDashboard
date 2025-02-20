ce0d6933c9aacd7a08f7c6fb489ccc2cea5b8038
= <=>
2025-02-20 10:26:55 -0300

    fix error TypeError: Additional arguments should be named <dialectname>_<argument>, got 'null'

--- app/models/emailmessage.py
+++ app/models/emailmessage.py
@@ -12,8 +12,8 @@ class Email_message(db.Model):
 
 
     id          = db.Column(db.Integer,primary_key=True)
-    title       = db.Column(db.String(50),null=False)
-    description = db.Column(db.Text,null=False)
+    title       = db.Column(db.String(50),nullable=False)
+    description = db.Column(db.Text,nullable=False)
     
     @staticmethod
     def get_all():
