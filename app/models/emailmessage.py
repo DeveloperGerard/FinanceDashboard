72c13af7ae181d3e5ad3e4735fa4b713dc122ee8
= <=>
2025-02-20 10:22:11 -0300

    Create model Email_messages for daily_email extra fuction

---
+++ app/models/emailmessage.py
@@ -0,0 +1,32 @@
+from app import db
+
+class Email_message(db.Model):
+    """
+    ⚠️El `usuario` no interectua con este modelo.⚠️\n
+    Objeto que representa los mensajes que se envian diariamente por `email`.\n
+    Contiene las columnas titulo,descripcion.
+    
+    """
+
+    __tablename__   = "email_messages"
+
+
+    id          = db.Column(db.Integer,primary_key=True)
+    title       = db.Column(db.String(50),null=False)
+    description = db.Column(db.Text,null=False)
+    
+    @staticmethod
+    def get_all():
+        """
+        Retorna todos los objetos del modelo `mensajes de email` en una lista.\n
+        :Ejemplo:
+        ```
+            return [email_message_1,email_message_2]
+        ```
+        """
+
+        all_messages = db.session.execute(db.select(Email_message)).scalars()
+        all_messages_list =[]
+        for message in all_messages:
+                all_messages_list.append(message)
+        return all_messages_list
--- app/models/importaciones.py
+++ app/models/importaciones.py
@@ -10,6 +10,8 @@ from ..models.loan_payment import Loan_payment
 from ..models.service import Service
 from ..models.service_payment import Service_payment
 from ..models.scheduled_incomes import Scheduled_income
+from ..models.emailmessage import Email_message
+Email_message
 Scheduled_income
 Account 
 Income
