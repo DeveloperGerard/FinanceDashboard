d922bd450bd5fa3584039f1b912e43e413cce6fd
MatiasRauque <rauquematias601@gmail.com>
2025-03-14 18:55:29 -0300

    fix-design-add-tabble

--- app/forms/form_loan.py
+++ app/forms/form_loan.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,IntegerField,DateField
+from wtforms import StringField,SubmitField,IntegerField,DateField,FloatField
 from wtforms.validators import DataRequired,Length,NumberRange,InputRequired
 
 class FormularioCrearPrestamos(FlaskForm):
@@ -7,8 +7,8 @@ class FormularioCrearPrestamos(FlaskForm):
     titular     = StringField('Titular',validators=[DataRequired(),Length(min=4,max=50)])
     descripcion = StringField("Descripcion",validators=[Length(0,160)])
     precio      = IntegerField('Precio',validators=[DataRequired()])
-    cuota       = IntegerField('Cuota',validators=[DataRequired(),NumberRange(1)])
-    tea         = IntegerField('Tea',validators=[NumberRange(min=0,max=100),InputRequired()],default=0)
+    cuota       = IntegerField('Cantidad de Cuotas',validators=[DataRequired(),NumberRange(1)])
+    tea         = FloatField('Tea',validators=[NumberRange(min=0,max=100),InputRequired()],default=0)
     fecha_vencimiento = DateField("Vencimiento",validators=[DataRequired()])
     submit      = SubmitField('Crear prestamo')
 
@@ -16,7 +16,7 @@ class FormularioActualizarPrestamos(FlaskForm):
     nombre   = StringField('Nombre',validators=[DataRequired(),Length(min=0,max=50)])
     titular  = StringField('Titular',validators=[DataRequired(),Length(min=4,max=50)])
     precio   = IntegerField('Precio',validators=[DataRequired()])
-    cuota    = IntegerField('Cuota',validators=[DataRequired()])
-    tea      = IntegerField('Tea',validators=[NumberRange(min=0,max=100)])
+    cuota    = IntegerField('Cantidad de Cuotas',validators=[DataRequired()])
+    tea      = FloatField('Tea',validators=[NumberRange(min=0,max=100)])
     fecha_vencimiento = DateField("Vencimiento",validators=[DataRequired()])
     submit   = SubmitField('Actualizar prestamo')
