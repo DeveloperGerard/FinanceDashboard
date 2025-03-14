505483efb26bd208ca131063e4cfee2e0ed112ce
MatiasRauque <rauquematias601@gmail.com>
2025-03-14 19:02:09 -0300

    add-margin

--- app/forms/form_account.py
+++ app/forms/form_account.py
@@ -6,7 +6,7 @@ class FormularioCrearCuenta(FlaskForm):
     nombre      = StringField('Nombre de la Cuenta',validators=[DataRequired(),Length(2,50)],
                               render_kw={"placeholder": "Ej: Cuenta Corriente"})
     tarjeta     = StringField('Número Cuenta/Tarjeta',validators=[DataRequired(),Length(2,50)],
-                              render_kw={"placeholder": "Ej: 1234-5678-9012-3456 / Recomendamos usar sólo los últimos 4 dígitos"})
+                              render_kw={"placeholder": "Ej: 1234 5678 9012 3456 / Recomendamos usar sólo los últimos 4 dígitos"})
     saldo       = IntegerField('Saldo Actual',validators=[DataRequired()],
                                render_kw={"placeholder": "Ej: 1000"})
     submit      = SubmitField('Crear cuenta')
@@ -14,5 +14,5 @@ class FormularioActualizarCuenta(FlaskForm):
     nombre      = StringField('Nombre de la Cuenta',validators=[Length(2,50)],
                               render_kw={"placeholder": "Ej: Cuenta Corriente"})
     tarjeta     = StringField('Número Cuenta/Tarjeta',validators=[DataRequired(),Length(2,50)],
-                              render_kw={"placeholder": "Ej: 1234-5678-9012-3456 / Recomendamos usar sólo los últimos 4 dígitos"})
+                              render_kw={"placeholder": "Ej: 1234 5678 9012 3456 / Recomendamos usar sólo los últimos 4 dígitos"})
     submit      = SubmitField('Actualizar cuenta')
